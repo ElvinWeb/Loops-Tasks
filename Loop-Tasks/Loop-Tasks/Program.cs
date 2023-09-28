eed481fb460d836f23302329264f6160bf290038
Elvin <elvinsrkrov@gmail.com>
2023-09-28 10:12:43 +0400

    loop tasks added

---
+++ Loop-Tasks/Loop-Tasks/Program.cs
@@ -0,0 +1,44 @@
+﻿#region Find Prime or not given number
+//int n = 22;
+//bool check = true;
+
+//for (int i = 2; i <= n / i - 1; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        check = false;
+//        break;
+//    }
+//    else
+//    {
+//        check = true;
+//    }
+
+//}
+
+//if (n == 0 || n == 1)
+//{
+//    Console.WriteLine($"Number {n} is not prime, and also not composite");
+
+//}
+//else if (check)
+//{
+//    Console.WriteLine($"{n} is Prime number");
+//}
+//else
+//{
+//    Console.WriteLine($"{n} is Composite Number ");
+//}
+#endregion
+
+#region Count digits of given number
+//int n = 3456;
+//int count = 0;
+
+//while (n > 0)
+//{
+//    n = n / 10;
+//    count++;
+//}
+//Console.WriteLine($"Output: {count}");
+#endregion
