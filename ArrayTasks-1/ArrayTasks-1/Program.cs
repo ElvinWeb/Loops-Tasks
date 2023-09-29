432193a4b19b9ed2ca68eda0c4c308b6b08f5fa7
Elvin <elvinsrkrov@gmail.com>
2023-09-29 07:25:02 +0400

    Array Tasks algorithms added

---
+++ ArrayTasks-1/ArrayTasks-1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+//Arrayde tek indeksde duran ededlerin hasilini tapin.
+#region ArrayTask-1
+//int[] numArr = new int[] { 19, 16, 5, 6, 8, 9 };
+//int multiply = 1;
+
+//for (int i = 0; i < numArr.Length; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        multiply *= numArr[i];
+//    }
+
+//}
+//Console.WriteLine($"Output: {multiply}");
+#endregion
+//1 soz verilir. Bu sozu tersine ceviren alqoritm yazin. (mes: Ters => sreT)
+#region ArrayTask-2
+//string word = "jvhhvvgutyHJGFGHFjkn";
+
+//for (int i = 1; i < word.Length + 1; i++)
+//{
+//    Console.Write(word[^i]);
+//}
+#endregion
+//Verilmis arrayde ededlerin ededi ortasini tapin.
+#region ArrayTask-3
+//int[] numArr = new int[] { 19, 16, 5, 6, 8, 9 };
+//double sum = 0;
+//for (int i = 0; i < numArr.Length; i++)
+//{
+//    sum += numArr[i];
+//}
+//double avg = sum / numArr.Length;
+
+//Console.WriteLine($"Output: {avg}");
+#endregion
