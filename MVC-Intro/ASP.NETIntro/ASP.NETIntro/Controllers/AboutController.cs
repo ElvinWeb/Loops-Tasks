00337b10a4521eb9a7b02f53180bd62808ea3732
Elvin <elvinsrkrov@gmail.com>
2023-12-03 15:28:31 +0400

    BookController and SliderController crud codes changed to Repository and Service arcitecture

---
+++ MVC-Intro/ASP.NETIntro/ASP.NETIntro/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETIntro.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public JsonResult AboutJson()
+        {
+            return Json(new { id = 1, Name = "ASP.NET MVC About",  language = "C#" });
+        }
+
+        public ContentResult AboutContent()
+        {
+            return Content("  Lorem ipsum dolor sit amet consectetur adipisicing elit.\n " +
+                " A asperiores possimus dolore aliquid? Quos aperiam quod libero minus veniam non temporibus est, " +
+                "autem neque?\r\n  Corporis odit facilis sed vel aut.Lorem ipsum dolor, sit amet consectetur adipisicing elit. \r\n" +
+                " Aliquam, porro. Lorem ipsum dolor sit, amet consectetur adipisicing elit.\n  " +
+                "Reiciendis, minima molestias rerum repellat aliquid minus nihil autem tempora ea optio.\n " +
+                "A asperiores possimus dolore aliquid? Quos aperiam quod libero minus veniam non temporibus est, autem neque?");
+        }
+    }
+}
---
+++ MVC-Intro/ASP.NETIntro/ASP.NETIntro/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ASP.NETIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string IndexWelcome()
+        {
+            return "Hello ASP.NET MVC";
+        }
+
+        public RedirectResult IndexDirect()
+        {
+            return Redirect("https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-7.0&tabs=visual-studio");
+        }
+    }
+}
